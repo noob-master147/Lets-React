b45a6557854dbfed2a85544114c5934ec620b950
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-09-30 23:28:01 +0530

    set up jSX in app.js and rendered it on the screen

--- Indecision App/public/scripts/app.js
+++ Indecision App/public/scripts/app.js
@@ -1,1 +1,10 @@
 console.log('app.js is running');
+
+// var template = <p>This is JSX from app.js</p>
+
+
+var temp = React.createElement("p", null, " This is a JSX");
+
+var appRoute = document.getElementById('app')
+
+ReactDOM.render(temp, appRoute)
