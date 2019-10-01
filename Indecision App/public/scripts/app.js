b4c2ac8c13a8b6bf6d2af950a117be8651b5cacf
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-10-01 15:05:05 +0530

    template a user and added values with an object

--- Indecision App/public/scripts/app.js
+++ Indecision App/public/scripts/app.js
@@ -1,12 +1,67 @@
 'use strict';
 
-console.log('app.js is running');
-
-var temp = React.createElement(
-  'p',
-  null,
-  'I am changing'
-);
 var appRoute = document.getElementById('app');
+//JSX JavaScript XML
+var temp = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This a paragraph'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Three'
+        )
+    )
+);
 
-ReactDOM.render(temp, appRoute);
+var user = {
+    userName: 'Divyansh Khandelwal',
+    userAge: 19,
+    userLocation: 'Vellore'
+
+    //Dynamic webabb
+};var newTemp = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.userName
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.userAge
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.userLocation
+    )
+);
+
+ReactDOM.render(newTemp, appRoute);
--- Indecision App/src/app.js
+++ Indecision App/src/app.js
@@ -1,6 +1,31 @@
-console.log('app.js is running');
-
-var temp = <p>I am changing</p>
 var appRoute = document.getElementById('app')
+//JSX JavaScript XML
+var temp = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This a paragraph</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+            <li>Item Three</li>
+        </ol>
+    </div>
+);
 
-ReactDOM.render(temp, appRoute)
+var user = {
+    userName : 'Divyansh Khandelwal',
+    userAge : 19,
+    userLocation : 'Vellore'
+}
+
+
+//Dynamic webabb
+var newTemp = (
+    <div>
+        <h1>{user.userName}</h1>
+        <p>Age: {user.userAge}</p>
+        <p>Location: {user.userLocation}</p>
+    </div>
+)
+
+ReactDOM.render(newTemp, appRoute)
