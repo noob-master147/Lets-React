7bd4f0e19beabdab2e5a1a0c89c1b93ec66a1a7e
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-10-03 21:39:58 +0530

    app for visibility on visibility branch

--- Indecision App/public/scripts/app.js
+++ Indecision App/public/scripts/app.js
@@ -3,80 +3,38 @@
 console.log('App is up and running!');
 var appRoute = document.getElementById('app');
 
-var title = {
-    title: 'Indecision App',
-    subTitle: 'Put your life in the hands of a computer',
-    options: []
+var state = false;
+
+var buttonClick = function buttonClick() {
+    state = !state;
+    renderVisibilityApp();
 };
-var onFormSubmit = function onFormSubmit(e) {
-    e.preventDefault();
-    var option = e.target.elements.option.value;
-    if (option) {
-        title.options.push(option);
-        e.target.elements.option.value = '';
-        renderTitleApp();
-    }
-};
-var removeAll = function removeAll() {
-    title.options = [];
-    renderTitleApp();
-};
-var makeDecision = function makeDecision() {
-    var randomNum = Math.floor(Math.random() * title.options.length);
-    var option = title.options[randomNum];
-    console.log(option);
-};
-var renderTitleApp = function renderTitleApp() {
-    var titleTemplate = React.createElement(
+
+var renderVisibilityApp = function renderVisibilityApp() {
+    var visibilityTemplate = React.createElement(
         'div',
         null,
         React.createElement(
             'h1',
             null,
-            title.title
-        ),
-        title.subTitle && React.createElement(
-            'p',
-            null,
-            title.subTitle
-        ),
-        React.createElement(
-            'p',
-            null,
-            title.options.length ? 'Here are your options' : 'No options'
+            'Visibility Toggle'
         ),
         React.createElement(
             'button',
-            { disabled: !title.options.length, onClick: makeDecision },
-            'Take my Decision'
+            { onClick: buttonClick },
+            state ? 'Hide Details' : 'Show Details'
         ),
-        React.createElement(
-            'button',
-            { onClick: removeAll },
-            'Remove All'
-        ),
-        React.createElement(
-            'form',
-            { onSubmit: onFormSubmit },
-            React.createElement('input', { type: 'text', name: 'option' }),
+        state && React.createElement(
+            'div',
+            null,
             React.createElement(
-                'button',
+                'p',
                 null,
-                'Add options'
+                'Here are some details'
             )
-        ),
-        React.createElement(
-            'ol',
-            null,
-            title.options.map(function (option) {
-                return React.createElement(
-                    'li',
-                    { key: option },
-                    option
-                );
-            })
         )
     );
-    ReactDOM.render(titleTemplate, appRoute);
+    ReactDOM.render(visibilityTemplate, appRoute);
 };
-renderTitleApp();
+
+renderVisibilityApp();
--- Indecision App/src/app.js
+++ Indecision App/src/app.js
@@ -1,50 +1,29 @@
 console.log('App is up and running!')
 const appRoute = document.getElementById('app')
 
-const title = {
-    title: 'Indecision App',
-    subTitle: 'Put your life in the hands of a computer',
-    options: []
+let state = false
+
+const buttonClick = () => {
+    state = !state
+    renderVisibilityApp()
 }
-const onFormSubmit = (e) => {
-    e.preventDefault();
-    const option = e.target.elements.option.value
-    if(option) {
-        title.options.push(option);
-        e.target.elements.option.value = ''
-        renderTitleApp()
-    }
-}
-const removeAll = () => {
-    title.options = []
-    renderTitleApp()
-}
-const makeDecision = () => {
-    const randomNum = Math.floor(Math.random() * title.options.length)
-    const option = title.options[randomNum]
-    console.log(option)
-}
-const renderTitleApp = () => {
-    const titleTemplate = (
+
+const renderVisibilityApp = () => {
+    const visibilityTemplate = (
         <div>
-            <h1>{title.title}</h1>
-            {title.subTitle && <p>{title.subTitle}</p>}
-            <p>{title.options.length ? 'Here are your options' : 'No options'}</p>
-            <button disabled={!title.options.length} onClick = {makeDecision}>Take my Decision</button>
-            <button onClick={removeAll}>Remove All</button>
-            <form onSubmit={onFormSubmit}>
-                <input type = "text" name = "option"></input>
-                <button>Add options</button>
-            </form>
-            <ol>
-                {
-                    title.options.map((option) => {
-                        return <li key = {option}>{option}</li>
-                    })
-                }
-            </ol>
+            <h1>Visibility Toggle</h1>
+            <button onClick={buttonClick}>
+                {state ? 'Hide Details' : 'Show Details'}
+            </button>
+            
+            {state && (
+                <div>
+                    <p>Here are some details</p>
+                </div>
+            )}
         </div>
-    );
-    ReactDOM.render(titleTemplate, appRoute)
+    )
+    ReactDOM.render(visibilityTemplate, appRoute)
 }
-renderTitleApp()
+
+renderVisibilityApp()
