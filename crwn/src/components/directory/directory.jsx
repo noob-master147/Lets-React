a2be21c5045428ba196e9c5b561d0ea4a2a0a8d6
Divyansh <divyanshkhandelwal147@gmail.com>
2020-10-12 19:44:12 +0530

    Update Shop page, collection preview and collection item components

--- crwn/src/components/directory/directory.jsx
+++ crwn/src/components/directory/directory.jsx
@@ -50,8 +50,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({ title, id, imageUrl, size }) => (
-                        <MenuItem key={id} title={title} image={imageUrl} size={size} />
+                    this.state.sections.map(({ id, ...otherSectionProps }) => (
+                        <MenuItem key={id} {...otherSectionProps} />
                     ))
                 }
             </div>
