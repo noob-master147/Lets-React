b8495903dbba46cbb7022e56d4c86f99c39ed2a5
Divyansh <divyanshkhandelwal147@gmail.com>
2020-10-12 20:04:59 +0530

    header component

--- crwn/src/App.js
+++ crwn/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import './App.css'
 
+import Header from './components/header/header'
 import HomePage from './pages/homepage/homepage'
 import ShopPage from './pages/shop/shop'
 
@@ -9,6 +10,7 @@ import ShopPage from './pages/shop/shop'
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
